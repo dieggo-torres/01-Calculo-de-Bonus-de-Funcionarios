86d3d1ab83356759532532bec89e7fa7441e0996
Diego Moura Torres <95168133+dieggo-torres@users.noreply.github.com>
2022-02-15 10:23:45 -0300

    Mudar regra de cálculo de bônus.

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 vendas_funcionario1 = 10000
 vendas_funcionario2 = 7700
 vendas_funcionario3 = 27000
-
-# Meta de vendas
-meta = 10000
+vendas_funcionario4 = 15000
 
 
 def calcular_bonus(vendas_funcionario):
     """Calcula o bônus que o funcionário receberá com base no valor total de suas vendas"""
-    if vendas_funcionario >= meta:
+    if vendas_funcionario >= 20000:
+        bonus_funcionario = vendas_funcionario * 0.15
+    elif vendas_funcionario >= 10000:
         bonus_funcionario = vendas_funcionario * 0.10
     else:
         bonus_funcionario = 0
@@ -21,8 +21,10 @@ def calcular_bonus(vendas_funcionario):
 bonus_funcionario1 = calcular_bonus(vendas_funcionario1)
 bonus_funcionario2 = calcular_bonus(vendas_funcionario2)
 bonus_funcionario3 = calcular_bonus(vendas_funcionario3)
+bonus_funcionario4 = calcular_bonus(vendas_funcionario4)
 
 # Exibição dos resultados
 print(f"Bônus do Funcionário 1: R${bonus_funcionario1}")
 print(f"Bônus do Funcionário 2: R${bonus_funcionario2}")
 print(f"Bônus do Funcionário 3: R${bonus_funcionario3}")
+print(f"Bônus do Funcionário 4: R${bonus_funcionario4}")
